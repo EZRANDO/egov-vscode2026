6a5502f4f918eb543769c37be856c7324625edef
nippyclouding <wonderwall09in@naver.com>
2026-05-03 19:03:49 +0900

    rm java.net.uri

--- myBatis-JPA-presentation/src/main/java/com/example/controller/MemberController.java
+++ myBatis-JPA-presentation/src/main/java/com/example/controller/MemberController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
