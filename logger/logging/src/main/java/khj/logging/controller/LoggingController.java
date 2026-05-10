7af70bc4e1709475183683cae36acff5aa0acc2a
kenu <kenu@okjsp.pe.kr>
2026-05-10 20:15:50 +0900

    Fix class name casing for LoggingController

--- logger/logging/src/main/java/khj/logging/controller/LoggingController.java
+++ logger/logging/src/main/java/khj/logging/controller/LoggingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class loggingController {
+public class LoggingController {
     @Autowired
     private calcService calcService;
 
